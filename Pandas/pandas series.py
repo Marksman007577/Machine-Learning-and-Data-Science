b1311e9868088c6c974b85ee0bf23d3716494f6a
Mark <omefemark@gmail.com>
2022-07-27 00:51:34 +0200

    Update pandas series.py

--- Pandas/pandas series.py
+++ Pandas/pandas series.py
@@ -12,3 +12,13 @@ print(my_ser)
 ages = {"Sam": 5, "Frank": 10, "Spike": 7}
 my_serr = pd.Series(ages)
 print(my_serr)
+
+#Adding two series in pandas
+Q1 = {'Japan': 80, 'China': 450, 'India': 200, 'USA': 250}
+Q2 = {'Brazil': 100, 'China': 500, 'India': 210, 'USA': 260}
+
+sales_Q1 = pd.Series(Q1)
+sales_Q2 = pd.Series(Q2)
+
+total_sale = sales_Q1.add(sales_Q2, fill_value=0)
+print(total_sale)
