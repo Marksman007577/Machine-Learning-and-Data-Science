a80a1f52a642a37db21b74d2560fd294c4a22ca4
Mark <omefemark@gmail.com>
2022-09-06 03:25:00 +0200

    initial commit

--- Dataframe/df_read_csv.py
+++
@@ -1,6 +0,0 @@
-# creating a dataframe reading from a CSV file
-import numpy as np
-import pandas as pd
-
-df = pd.read_csv("C:\\Users\\User\\Desktop\\Machine Learning and Data Science\\UNZIP_FOR_NOTEBOOKS_FINAL\DATA\\tips.csv")
-print(df)
